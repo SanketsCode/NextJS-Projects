fb465bc42a5117fb0d092c28d2ecc3d9234e121a
sanket <programmersanket@gmail.com>
2022-12-01 17:14:19 +0530

    Creation Layout header and footer are done

--- DJ-Events/dj-events-frontend/pages/events/[slug].js
+++ DJ-Events/dj-events-frontend/pages/events/[slug].js
@@ -1,9 +1,20 @@
-import React from 'react'
+import Head from "next/head";
+import { useRouter } from "next/router"
+import Layout from "../../components/Layout";
+
 
 export default function EventPage() {
+    const router = useRouter();
+
   return (
-    <div>
+    <Layout>
+        <Head>
+            <title>DJ Events</title>
+            <meta name="description" content="welcome to DJ Events" />
+        </Head>
         <h1>My Event</h1>
-    </div>
+        <h3>{router.query.slug}</h3>
+        <button onClick={() => router.push('/')}>click</button>
+    </Layout>
   )
 }
